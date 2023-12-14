9435bca0644e477db0a2520a1aea7d960ec234ea
ani <anitabijari@gmail.com>
2023-12-14 14:13:04 +0100

    update powerpoint

--- client/main.powerpoints.js
+++ client/main.powerpoints.js
@@ -1,25 +1,25 @@
 async function search() {
-    let searchTerm = document.forms.searchForm.term.value;
-    let searchType = document.forms.searchForm.searchType.value;
-    console.log(searchType);
-    document.forms.searchForm.term.value = '';
-    let rawData = await fetch('/api/PowerPoint/' + searchTerm + '/' + searchType);
-    let powerpoints = await rawData.json();
-    let html = `
-      <p>You search for "${searchTerm}"...</p>
-      <p>You found ${powerpoints.length} results.</p>
+  let searchTerm = document.forms.searchForm.term.value;
+  let searchType = document.forms.searchForm.searchType.value;
+  console.log(searchType);
+  document.forms.searchForm.term.value = '';
+  let rawData = await fetch('/api/powerpoint/' + searchTerm + '/' + searchType);
+  let powerpoints = await rawData.json();
+  let html = `
+    <p>You search for "${searchTerm}"...</p>
+    <p>You found ${powerpoints.length} results.</p>
+  `;
+  for (let PowerPoint of powerpoints) {
+    let meta = PowerPoint.PowerPoint_metadata;
+    html += `
+      <section>
+        <h2>${meta.title}</h2>
+        <p><b>company:</b> ${meta.company}</p>
+        <p><b>Document:</b> ${meta.urlkey}</p> 
+        <a href="powerpoints/${PowerPoint.PowerPoint_fileName}" target="_blank">View the powerpoint...</a>
+      </section>
     `;
-    for (let PowerPoint of powerpoints) {
-      let meta = PowerPoint.metadata;
-      html += `
-        <section>
-          <h2>${meta.title}</h2>
-          <p><b>company:</b> ${meta.company}</p>
-          <p><b>Document:</b> ${meta.urlkey}</p> 
-          <p><embed src="powerpoints/${powerpoints.fileName}" width='60%' height='150px' frameborder='0'></p>
-        </section>
-      `;
-    }
-    let searchResultsElement = document.querySelector('.searchResults');
-    searchResultsElement.innerHTML = html;
   }
+  let searchResultsElement = document.querySelector('.searchResults');
+  searchResultsElement.innerHTML = html;
+}
