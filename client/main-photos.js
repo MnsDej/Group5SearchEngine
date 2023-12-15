60c9c230df2a58f4d9160536bed21545de3ff449
MnsDej <mansdejbo@hotmail.com>
2023-12-15 13:16:01 +0100

    CSS Style Photos

--- client/main-photos.js
+++ client/main-photos.js
@@ -20,6 +20,9 @@ async function search() {
     for(let photo of photos) {
       html += `
         <section>
+        <p class="phototext"><b>ISO:</b> ${photo.Photo_metadata.ISO}</p>
+        <p class="phototext"><b>Make:</b> ${photo.Photo_metadata.Make}</p>
+        <p class="phototext"><b>Model:</b> ${photo.Photo_metadata.Model}</p>
         <a href="https://maps.google.com/?q=${photo.Photo_metadata.latitude},${photo.Photo_metadata.longitude}" target="_blank"><img src="photos/${photo.Photo_filename}"></a>
         </section>
     
